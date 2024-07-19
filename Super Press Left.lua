904f2fc4b6002190ef50889b284b5dde97bcb996
Nathan <coolndr@rocketmail.com>
2024-07-18 23:35:02 -0500

    Update Super Press Left.lua
    Got stay still and press down endings working in p8

--- Super Press Left.lua
+++ Super Press Left.lua
@@ -1,34 +1,35 @@
 function _init()
 	-- variable declarations
 	stage = 0
-	maxtimer = 30 * 30 --seconds * frames
+	maxtimer = 10 * 30 --seconds * frames
 	timer = maxtimer
+	bgcolor=2
 end--of _init()
 
 function _update()
 	-- stage 0 is initial splash screen
-	if stage = 0 then
+	if stage == 0 then
 		-- tick down timer if nothing is pressed...
 		-- ...but reset it if something is pressed.
 		timer = btn() < 1 and timer-1 or maxtimer
 		
 		-- check for "press nothing" ending
-		if timer < 1 then cutscene(3) end
+		if timer < 1 then cutscene(7) end
 		
-		-- todo: check for left presses for the "meet the eye E" ending
+		-- todo: check for left presses for the "meet ☉" ending
 		
 		-- finally, check for a down press (like any sane developer would've done first)
-		if btn(D) then stage = 1 end
-		-- make sure capital d D means down		
+		if btn(⬇️) then cutscene(1) end
 	end--of stage 0
 end--of _update()
 
 function _draw() --placeholder
-	cls(1)
+	cls(bgcolor)
 	circfill(64,64,32,stage)
 end--of _draw()
 
 function cutscene(s)
 	-- todo: write a handler for the ending screens
-	stage = 9
+	stage = s
+	bgcolor = 3
 end--of cutscene()
